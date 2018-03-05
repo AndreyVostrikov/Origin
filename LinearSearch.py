8376ea3a5d49d0de2c0c5e2245895dce7e38d915
AndreyVostrikov <vostrikov99@yandex.ru>
2018-03-05 11:38:09 +0300

    Линейная сортировка

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,5 +1,5 @@
 lst = [45, 444, 46, 666, 2, 3, 9, 90, 80, 4, 10]           # Линейный поиск
-n = 890
+n = 9
 index = 0
 x = len(lst) - 1
 while lst[index] != n and x > index:
@@ -7,4 +7,4 @@ while lst[index] != n and x > index:
 if lst[index] == n:
     print(index)
 else:
-    print('Элемент не найден')   
+    print('Элемент не найден')
