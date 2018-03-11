04e00eea8770f24a38503a9ce7cd7f05fcb8dcb1
AndreyVostrikov <vostrikov99@yandex.ru>
2018-03-12 01:22:29 +0300

    Linear_Search and Binary_Search final version

--- LinearSearch.py
+++ LinearSearch.py
@@ -7,15 +7,15 @@ def linear_search(number):
     lst = [int(input('1)Введите число:')), int(input('2)Введите число:')), int(input('3)Введите число:')),
            int(input('4)Введите число:')), int(input('5)Введите число:')), int(input('6)Введите число:')),
            int(input('7)Введите число:')), int(input('8)Введите число:')), int(input('9)Введите число:')),
-           int(input('10)Введите число:'))]
+           int(input('10)Введите число:'))]     # Ввод в список 10-ти значений
     index = 0
     x = len(lst) - 1
-    while lst[index] != number and x > index:
+    while lst[index] != number and x > index:   # В данном циле проводится перебор каждого элемента по порядку
         index += 1
-    if lst[index] == number:
+    if lst[index] == number:                    # Если элемент равен искомому - выводим его индекс
         print('Индекс:', index)
-    else:
+    else:                                       # В противном случае - выводим "Элемент не найден"
         print('Элемент не найден')
 
 
-print(linear_search(3))
+print(linear_search(80))
