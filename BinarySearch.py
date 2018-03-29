1e5cefc25ff0534d9a526b4503dc60043d5f16d6
AndreyVostrikov <vostrikov99@yandex.ru>
2018-03-29 13:59:24 +0300

    Binary Search

--- BinarySearch.py
+++ BinarySearch.py
@@ -7,8 +7,8 @@ DEBUG_MODE = False
 # Constant strings for debugging
 DEBUG_MSG_INPUT_LIST = ["Input list: ", "Исходный список: "]
 DEBUG_MSG_OUTPUT_LIST = ["Output list: ", "Отсортированный список: "]
-
-
+LANGUAGE_OF_OUTPUT = ['Element not found', 'Элемент не найден']
+INDEX_ELEMENT = ['index of element', 'Индекс элемента']
 # Language selection
 LANG_EN_CODE = 0
 LANG_RU_CODE = 1
@@ -16,7 +16,7 @@ LANG_RU_CODE = 1
 
 def binary_search(input_element, language = LANG_EN_CODE):
     element = input_element
-    lst = SortBySelection.sort_by_selection([245, 23, 423, 24, 23, 56, 23])
+    lst = SortBySelection.sort_by_selection([10, 234, 25, 52, 546])
     if DEBUG_MODE: print(DEBUG_MSG_OUTPUT_LIST[language], lst)
     left_border_list = 0
     right_border_list = len(lst)
@@ -27,11 +27,11 @@ def binary_search(input_element, language = LANG_EN_CODE):
         else:
             left_border_list = middle_list
     if lst[left_border_list] == element:
-        return left_border_list
+        return print(INDEX_ELEMENT[language], left_border_list)
     elif lst[right_border_list] != element:
-        return 'Element not found'
+        return print(LANGUAGE_OF_OUTPUT[language])
     else:
-        return right_border_list
+        return print(INDEX_ELEMENT[language], right_border_list)
 
 
-print(binary_search(23))
+print(binary_search(522))
