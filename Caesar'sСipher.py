809eb5d09e3d961ef8b93790cfa75b458a749590
AndreyVostrikov <vostrikov99@yandex.ru>
2018-05-21 14:12:33 +0300

    Task 5

--- Caesar'sСipher.py
+++ Caesar'sСipher.py
@@ -5,8 +5,8 @@ def caesar_cipher():
     word = str(input('Enter the word, which you want to encrypt: '))
     result = ''
     for index_word in word:
-        result += alphabet[(alphabet.index(index_word) + key)]
+        result += alphabet[(alphabet.index(index_word) + key) % len(alphabet)]
     return result
 
 
-print(caesar_cipher())
+print(caesar_cipher()) 
